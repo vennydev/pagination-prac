acde49f261c4d8be72a2a8d1c1a4c390b6998bd0
venny <hs421@naver.com>
2022-10-12 17:16:35 +0900

    feat: parse contacts

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <pagination-list :list-array="pageArray" />
+    <pagination-numbers :total-rows="total" @clicked="paginateItems" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
+import { Component, Vue } from "vue-property-decorator";
+import PaginationList from "./components/PaginationList.vue";
+import PaginationNumbers from "./components/PaginationNumbers.vue";
+import axios from "axios";
 
 @Component({
   components: {
-    HelloWorld,
+    PaginationList,
+    PaginationNumbers,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  public pageArray: any = [];
+  public total = 0;
+  public paginatedItems = [];
+  public perPage = 12;
+
+  public created() {
+    axios
+      .get("http://sample.bmaster.kro.kr/contacts")
+      .then((response: any) => {
+        const contacts = response.data.contacts;
+        this.pageArray = contacts;
+        this.total = contacts.length;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  public paginateItems(current_Page: any, perPage: any) {
+    console.log("current_Page: ", current_Page);
+    console.log("perPage: ", this.perPage);
+    // const itemsToParse = this.pageArray;
+  }
+
+  // 첫 마운트 시
+  public mounted(): void {
+    // this.paginateItems();
+  }
+
+  // paginated 순서
+  // 첫 mount시 보여줄 데이터 구현
+  // 버튼 눌렀을 때 보여줄 데이터 구현
+}
 </script>
 
 <style>
---
+++ src/components/PaginationList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-table-simple hover small resoponsive>
+      <b-thead head-variant="light">
+        <b-tr>
+          <b-th>NO</b-th>
+          <b-th>TEL</b-th>
+          <b-th>ADDRESS</b-th>
+          <b-th>NAME</b-th>
+        </b-tr>
+      </b-thead>
+      <b-tbody>
+        <b-tr v-for="list in listArray" :key="list.no">
+          <b-th>{{ list.no }}</b-th>
+          <b-th>{{ list.tel }}</b-th>
+          <b-th>{{ list.address }}</b-th>
+          <b-th>{{ list.name }}</b-th>
+        </b-tr>
+      </b-tbody>
+    </b-table-simple>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    listArray: {
+      type: Array,
+      required: true,
+    },
+  },
+  created() {
+    console.log("pageArray: ", this.listArray);
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
